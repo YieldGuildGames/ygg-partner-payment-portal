ad89b6ba5f05efc1e13d83d2d43cb8c9cbc28cb6
Varun Sharma <varun_sharma@live.co.uk>
2024-11-14 11:01:47 -0800

    fix: wrong data for paymetn portals testing and fow

--- app/payment/game-testing-package/page.tsx
+++ app/payment/game-testing-package/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { FUTURE_OF_WORK_PAYMENT_INFO } from "@/utils/constants/paymentInfo.constants";
-import PaymentInfoComponent from "@/components/PaymentPortal/PaymentPortal";
+'use client';
+import { GAME_TESTING_PAYMENT_INFO } from '@/utils/constants/paymentInfo.constants';
+import PaymentInfoComponent from '@/components/PaymentPortal/PaymentPortal';
 
 export default function GAPPaymentPage() {
-  return (
-    <PaymentInfoComponent SELLER_PAYMENT_INFO={FUTURE_OF_WORK_PAYMENT_INFO} />
-  );
+    return <PaymentInfoComponent SELLER_PAYMENT_INFO={GAME_TESTING_PAYMENT_INFO} />;
 }
