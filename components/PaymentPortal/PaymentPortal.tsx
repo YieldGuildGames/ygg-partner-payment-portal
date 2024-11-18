275837f0282de86aec0b7df1e645a16c01b7beb3
Varun Sharma <varun_sharma@live.co.uk>
2024-11-18 14:13:16 -0800

    feat: enable test mode for payment embed

--- components/PaymentPortal/PaymentPortal.tsx
+++ components/PaymentPortal/PaymentPortal.tsx
@@ -1,57 +1,61 @@
-"use client";
-import React from "react";
-import { Link } from "@nextui-org/link";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title } from "@/components/primitives";
-import { PayEmbed } from "thirdweb/react";
-import { client } from "@/config/Thirdweb/client";
-import { SellerPaymentInfoInterface } from "@/types/PaymentInfo.types";
-
+'use client';
+import React from 'react';
+import { Link } from '@nextui-org/link';
+import { button as buttonStyles } from '@nextui-org/theme';
+import { siteConfig } from '@/config/site';
+import { title } from '@/components/primitives';
+import { PayEmbed } from 'thirdweb/react';
+import { client } from '@/config/Thirdweb/client';
+import { SellerPaymentInfoInterface } from '@/types/PaymentInfo.types';
+import { TEST_MODE } from '@/utils/constants/global.constants';
 interface PaymentInfoComponentProps {
-  SELLER_PAYMENT_INFO: SellerPaymentInfoInterface; // Adjust the type according to the structure of GAP_PAYMENT_INFO
+    SELLER_PAYMENT_INFO: SellerPaymentInfoInterface; // Adjust the type according to the structure of GAP_PAYMENT_INFO
 }
 
-const PaymentInfoComponent: React.FC<PaymentInfoComponentProps> = ({
-  SELLER_PAYMENT_INFO,
-}) => {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Welcome to the&nbsp;</span>
-        <span className={title({ color: "violet" })}>Partner&nbsp;</span>
-        <br />
-        <span className={title()}>Payment Portal</span>
-      </div>
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "sm",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.partnerDashboard}
-        >
-          Back to Partner Dashboard
-        </Link>
-      </div>
+const PaymentInfoComponent: React.FC<PaymentInfoComponentProps> = ({ SELLER_PAYMENT_INFO }) => {
+    return (
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <div className="inline-block max-w-xl text-center justify-center">
+                <span className={title()}>Welcome to the&nbsp;</span>
+                <span className={title({ color: 'violet' })}>Partner&nbsp;</span>
+                <br />
+                <span className={title()}>Payment Portal</span>
+            </div>
+            <div className="flex gap-3">
+                <Link
+                    isExternal
+                    className={buttonStyles({
+                        color: 'primary',
+                        radius: 'sm',
+                        variant: 'shadow'
+                    })}
+                    href={siteConfig.links.partnerDashboard}
+                >
+                    Back to Partner Dashboard
+                </Link>
+            </div>
 
-      <PayEmbed
-        client={client}
-        payOptions={{
-          mode: "direct_payment",
-          paymentInfo: SELLER_PAYMENT_INFO.paymentInfo,
-          metadata: SELLER_PAYMENT_INFO.metadata,
-        }}
-        connectOptions={{
-          connectModal: {
-            size: "wide",
-          },
-        }}
-      />
-    </section>
-  );
+            <PayEmbed
+                client={client}
+                payOptions={{
+                    mode: 'direct_payment',
+                    paymentInfo: SELLER_PAYMENT_INFO.paymentInfo,
+                    metadata: SELLER_PAYMENT_INFO.metadata,
+                    buyWithCrypto: {
+                        testMode: TEST_MODE
+                    },
+                    buyWithFiat: {
+                        testMode: TEST_MODE
+                    }
+                }}
+                connectOptions={{
+                    connectModal: {
+                        size: 'wide'
+                    }
+                }}
+            />
+        </section>
+    );
 };
 
 export default PaymentInfoComponent;
