74088042beeaa887b1126a353ce05292e6164fe7
Varun Sharma <varun_sharma@live.co.uk>
2024-11-19 12:38:29 -0800

    feat: implement discount code feature

--- components/PaymentPortal/PaymentPortal.tsx
+++ components/PaymentPortal/PaymentPortal.tsx
@@ -1,19 +1,50 @@
 'use client';
-import React from 'react';
-import { Link } from '@nextui-org/link';
-import { button as buttonStyles } from '@nextui-org/theme';
-import { siteConfig } from '@/config/site';
+import React, { useState } from 'react';
 import { title } from '@/components/primitives';
 import { PayEmbed } from 'thirdweb/react';
 import { client } from '@/config/Thirdweb/client';
 import { SellerPaymentInfoInterface } from '@/types/PaymentInfo.types';
 import { TEST_MODE } from '@/utils/constants/paymentInfo.constants';
+import { Input } from '@nextui-org/input';
+import { Popover, PopoverTrigger, PopoverContent, Button } from '@nextui-org/react';
+import { DISCOUNT_CODES } from '@/utils/constants/discountCodes.constants';
 
 interface PaymentInfoComponentProps {
     SELLER_PAYMENT_INFO: SellerPaymentInfoInterface; // Adjust the type according to the structure of GAP_PAYMENT_INFO
 }
 
 const PaymentInfoComponent: React.FC<PaymentInfoComponentProps> = ({ SELLER_PAYMENT_INFO }) => {
+    const [discountCode, setDiscountCode] = useState('');
+    const [sellerPaymentInfo, setSellerPaymentInfo] = useState(SELLER_PAYMENT_INFO);
+    const [discountApplied, setDiscountApplied] = useState(false);
+
+    // TODO: implement a more secure version for discount code checking, for an MVP this will do for now. We'll implement something on server side.
+    // We have the partnership team and Marielle checking and approving the Quest packages for total rewards, contract signign etc, so there's a human in the loop to verify if anyone abuses this anyways.
+
+    const handleDiscountCodeChange = (e: any) => {
+        setDiscountCode(e.target.value);
+        setDiscountApplied(false);
+    };
+
+    const applyDiscount = () => {
+        const discountPercentage = DISCOUNT_CODES[discountCode.toUpperCase()];
+        if (discountPercentage) {
+            const originalAmount = parseFloat(SELLER_PAYMENT_INFO.paymentInfo.amount);
+            const discountAmount = (originalAmount * discountPercentage) / 100;
+            const newAmount = (originalAmount - discountAmount).toFixed(2);
+            setSellerPaymentInfo({
+                ...SELLER_PAYMENT_INFO,
+                paymentInfo: {
+                    ...SELLER_PAYMENT_INFO.paymentInfo,
+                    amount: newAmount
+                }
+            });
+            setDiscountApplied(true);
+        } else {
+            alert('Invalid discount code');
+        }
+    };
+
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
             <div className="inline-block max-w-xl text-center justify-center">
@@ -22,26 +53,37 @@ const PaymentInfoComponent: React.FC<PaymentInfoComponentProps> = ({ SELLER_PAYM
                 <br />
                 <span className={title()}>Payment Portal</span>
             </div>
-            <div className="flex gap-3">
-                <Link
-                    isExternal
-                    className={buttonStyles({
-                        color: 'primary',
-                        radius: 'sm',
-                        variant: 'shadow'
-                    })}
-                    href={siteConfig.links.partnerDashboard}
-                >
-                    Back to Partner Dashboard
-                </Link>
+
+            <div className="flex flex-wrap md:flex-nowrap gap-4">
+                <Input
+                    type="text"
+                    label="Discount Code: "
+                    value={discountCode}
+                    placeholder="Enter discount code"
+                    className="max-w-xs"
+                    onChange={handleDiscountCodeChange}
+                />
+                <Popover placement="bottom" showArrow={true}>
+                    <PopoverTrigger>
+                        <Button onClick={applyDiscount} color="secondary" className="mt-2">
+                            Apply
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <div className="px-1 py-2">
+                            <div className="text-small font-bold">{discountApplied && <p>Discount applied!</p>}</div>
+                        </div>
+                    </PopoverContent>
+                </Popover>
             </div>
 
+            {discountApplied && <p>Discount applied! New amount: ${sellerPaymentInfo.paymentInfo.amount}</p>}
             <PayEmbed
                 client={client}
                 payOptions={{
                     mode: 'direct_payment',
-                    paymentInfo: SELLER_PAYMENT_INFO.paymentInfo,
-                    metadata: SELLER_PAYMENT_INFO.metadata,
+                    paymentInfo: sellerPaymentInfo.paymentInfo,
+                    metadata: sellerPaymentInfo.metadata,
                     buyWithCrypto: {
                         testMode: TEST_MODE
                     },
---
+++ utils/constants/discountCodes.constants.ts
@@ -0,0 +1,11 @@
+// Define the type for discount codes
+type DiscountCodesType = {
+    [key: string]: number;
+};
+
+export const DISCOUNT_CODES: DiscountCodesType = {
+    SAVE10: 10,
+    SAVE20: 20,
+    SAVE30: 30,
+    YGG_PARTNER_DASHBOARD_LAUNCH: 10
+};
