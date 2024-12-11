fa37bb58aefd88f1e06c993b1127d79fac2c3bde
Varun Sharma <varun_sharma@live.co.uk>
2024-12-11 16:27:41 -0500

    feat: add working base jest testing layer

---
+++ config/jest/jest.config.js
@@ -0,0 +1,26 @@
+const esmModules = ['uuid'];
+
+module.exports = {
+    rootDir: '../../',
+    // jest.config.js
+    setupFiles: ['config/config'],
+    setupFilesAfterEnv: ['./config/jest/jest.setup.js'],
+    transform: {
+        '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { configFile: './.babelrc' }],
+
+        '^.+\\.svg$': '<rootDir>/svgTransform.js'
+    },
+    moduleNameMapper: {
+        '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/identity-obj-proxy',
+        '^@components(.*)$': '<rootDir>/components$1',
+        '^@pages(.*)$': '<rootDir>/pages$1',
+        '^@hooks(.*)$': '<rootDir>/context/hooks$1'
+    },
+    verbose: true,
+    collectCoverage: true,
+    coveragePathIgnorePatterns: ['<rootDir>/test/test-utils.js'],
+    testPathIgnorePatterns: ['<rootDir>/cypress/integration'],
+    // ignore cypress
+    modulePathIgnorePatterns: ['./cypress'],
+    transformIgnorePatterns: [`node_modules/(?!(?:.pnpm/)?(${esmModules.join('|')}))`]
+};
