3cb6a0d01d39ca48e07fb8699b1990c41dc7fc4d
Varun Sharma <varun_sharma@live.co.uk>
2024-12-12 17:00:14 -0500

    feat: add posthog integration cleint side

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from './providers';
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/config/fonts';
 import { Navbar } from '@/components/navbar';
+import { ClientPostHogProvider } from './providers/posthog.provider';
 
 export const metadata: Metadata = {
     title: {
@@ -28,15 +29,17 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html suppressHydrationWarning lang="en">
-            <head />
-            <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-                <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-                    <div className="relative flex flex-col h-screen">
-                        <Navbar />
-                        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
-                    </div>
-                </Providers>
-            </body>
+            <ClientPostHogProvider>
+                <head />
+                <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+                    <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+                        <div className="relative flex flex-col h-screen">
+                            <Navbar />
+                            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
+                        </div>
+                    </Providers>
+                </body>
+            </ClientPostHogProvider>
         </html>
     );
 }
